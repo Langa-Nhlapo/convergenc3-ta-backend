9c2552d9ba82b608222c079302841c75b947b53a
Langa <lsnhlapo@gmail.com>
2022-08-08 07:26:59 +0200

    Getting the server ready for production

--- src/index.js
+++ src/index.js
@@ -64,6 +64,13 @@ mongoose
 
 app.use('/user', UserRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
+
 app.listen(PORT, (err) => {
   if (err) {
      return console.log('Unable to start server')
